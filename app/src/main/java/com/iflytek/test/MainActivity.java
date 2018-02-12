aeb43eb956e8e25750340725836dd8e3069ddbbd
jiwang <jiwang@iflytek.com>
2018-02-12 08:42:13 +0800

    [分支]修改内容

--- app/src/main/java/com/iflytek/test/MainActivity.java
+++ app/src/main/java/com/iflytek/test/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btn_test1).setOnClickListener(this);
         mTestSync = new TestSync();
         mTestSync1 = new TestSync();
-        //创建分支develop,init
+        //创建分支develop,this is develop change
 
 
     }
