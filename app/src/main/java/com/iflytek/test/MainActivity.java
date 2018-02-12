80d2166088133255069749f054a53ce8881ed1a4
jiwang <jiwang@iflytek.com>
2018-02-12 09:34:09 +0800

    [develop]提交新类

--- app/src/main/java/com/iflytek/test/MainActivity.java
+++ app/src/main/java/com/iflytek/test/MainActivity.java
@@ -2,9 +2,11 @@ package com.iflytek.test;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
     private TestSync mTestSync;
     private TestSync mTestSync1;
 
@@ -16,10 +18,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btn_test1).setOnClickListener(this);
         mTestSync = new TestSync();
         mTestSync1 = new TestSync();
-        //创建分支develop,init this is master
         //创建分支develop,this is develop change
+    }
 
-
+    public void inDevelopMethod() {
+        Log.d(TAG, "this is a develop project");
     }
 
     @Override
