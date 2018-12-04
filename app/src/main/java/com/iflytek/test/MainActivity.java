e96b0e7fb90d4c816abea74f2f5536c0a4a26e98
jiwang <jiwang@iflytek.com>
2018-12-04 10:13:13 +0800

    Revert "[修改代码] 测试提交"
    This reverts commit d9de584

--- app/src/main/java/com/iflytek/test/MainActivity.java
+++ app/src/main/java/com/iflytek/test/MainActivity.java
@@ -16,19 +16,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         //测试提交
-
-        //测试提交
-
-        //测试提交1
-
-        //测试提交2 mixed 可以回退commit,从新提交 Hard 不仅回退commit,并且代码也回退
-
-        //测试3
-
-        //测试reset
-
-        //测试interactivity
-
-        //check out version
     }
 }
