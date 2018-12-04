f4766f47ce061752765f890cebea0b0473575f4c
jiwang <jiwang@iflytek.com>
2018-12-04 15:56:59 +0800

    Revert "Revert "[修改代码] revert test""
    This reverts commit c8d41e8dcb61530e194d393d872d679c5456f002.

--- app/src/main/java/com/iflytek/test/MainActivity.java
+++ app/src/main/java/com/iflytek/test/MainActivity.java
@@ -25,6 +25,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 可以再使用reset current branch to here soft 方式,将这次revert合并
         // 本地代码变动
         //会弹出弹框选择第一个会丢弃当前代码,选择第二个会保存当前代码
-        // 测试选择第二个
+        // 测试选择第二个,会保存当前代码吗,会保存
     }
 }
